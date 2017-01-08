14fada546950622757a205f2d643cd2038554f84
abdoul <ndiayeabdoul870@gmail.com>
2017-01-08 01:03:17 +0100

    ajout partie abdoul

--- FinSo/src/AdministrationBundle/Controller/DefaultController.php
+++ FinSo/src/AdministrationBundle/Controller/DefaultController.php
@@ -8,6 +8,24 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AdministrationBundle:Default:index.html.twig');
+    	$em = $this->getDoctrine()->getManager();
+        $projet = $em->getRepository('AccueilBundle:Projet')->findAll();
+        return $this->render('AdministrationBundle:Default:index.html.twig', array('projet' => $projet));
     }
+    public function afficherprojetAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $projet = $em->getRepository('AccueilBundle:Projet')->findOneBy( array('id' => $id));;
+        return $this->render('AdministrationBundle:Default:affiche.html.twig', array('projet' => $projet));
+    }
+
+     public function validationAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $projet = $em->getRepository('AccueilBundle:Projet')->findOneBy( array('id' => $id));
+        $validation=new Validation();
+        
+        return $this->render('AdministrationBundle:Default:affiche.html.twig', array('projet' => $projet));
+    }
+
 }
